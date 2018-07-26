4ce418215c9c62eb15377eefd15fe4e446b07e98
Ryan Reynolds <rreynolds9@fordham.edu>
2018-07-26 14:26:01 -0400

    Debugging

--- Homeworks/HW2 Mutex/Sumsqr.c
+++ Homeworks/HW2 Mutex/Sumsqr.c
@@ -1,37 +1,53 @@
-Assignment 2
-#include <studio.h>
-#include <math.h>
-#include<pthread.h> 
-#include<stdlib.h> 
-#include<unistd.h> 
- 
-Int start_points[4];
+
+#include <stdio.h>
+#include <pthread.h> 
+#include "math.h"
+#include <stdlib.h> 
+#include <unistd.h> 
+
+
+int start_points[4];
 double ssum;
-Pthread_mutex_t sumlock;
-void *sumSqrt( int start, int end){
+pthread_mutex_t sumlock;
+
+
+double sumSqrt( int start, int end){
 double psum=0;
-for(int i = start;i<end; i ++){
-          psum+=sqrt(i);
+for(;start<end; start++){
+	psum=psum+sqrt(start);
 }
-Pthread_mutex_t_lock(&sumlock);
+//pthread_mutex_lock(&sumlock);
 ssum+=psum;
-Pthread_mutex_t_unlock(&sumlock);
+//pthread_mutex_unlock(&sumlock);
 
 }
 
-void main(int argc, char *argv[]){
+int main(int argc, char *argv[]){
+if (argc<2){
+	printf("No number entered");
+	return EXIT_FAILURE;
+}
 
-int n = atoi(argv[2]);
+int n = atoi(argv[1]);
+printf("\n%i\n",n);
 pthread_t t1, t2;
-
+/*
 start_points[0]=1;
 start_points[1]=n/3+1;
-start_points[2]=2n/3+1;
+*/
+start_points[2]=2*n/3+1;
 start_points[3]=n+1;
-pthread_create(&t1, NULL, sumSqrt(start_points[1], start_points[2]), NULL);
-pthread_create(&t2, NULL, sumSqrt(start_points[2], start_points[3]), NULL);
-sumSqrt(start_points[0], start_points[1]);
+printf("\n%i\n",start_points[2]);
+printf("\n%i\n",start_points[3]);
+/*pthread_create(&t1, NULL, sumSqrt(start_points[1], start_points[2]), NULL);*/
+double res=sumSqrt(start_points[2], start_points[3]);
+//pthread_create(&t2, NULL, sumSqrt(start_points[2], start_points[3]), NULL);
+/*sumSqrt(start_points[0], start_points[1]);
 pthread_join(t1,NULL);
 pthread_join(t2,NULL);
-printf(“ \nThe sum of the sqrts of %i is %d” n, psum);
+//printf("\nThe sum of the sqrts of %i is %d", n, ssum);
+*/
+
+printf("%d\n", ssum);
+return 0;
 }
