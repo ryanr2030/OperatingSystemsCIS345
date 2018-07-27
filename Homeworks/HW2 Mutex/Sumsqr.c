7e26f2f01eb472b9a5b639ea637df98351a80536
Ryan Reynolds <rreynolds9@fordham.edu>
2018-07-26 20:39:15 -0400

    Debug: sqrt not working

--- Homeworks/HW2 Mutex/Sumsqr.c
+++ Homeworks/HW2 Mutex/Sumsqr.c
@@ -6,48 +6,59 @@
 #include <unistd.h> 
 
 
-int start_points[4];
-double ssum;
+int sp1[2];
+int sp2[2];
+int sp3[2];
+double ssum=0;
 pthread_mutex_t sumlock;
 
 
-double sumSqrt( int start, int end){
-double psum=0;
-for(;start<end; start++){
-	psum=psum+sqrt(start);
-}
-//pthread_mutex_lock(&sumlock);
-ssum+=psum;
-//pthread_mutex_unlock(&sumlock);
+void * sumSqrt(void* i){
+	double psum=0;
+	int *start=i;
+	printf("%i ",start[1]);
+	for(int i=start[0];i<start[1]; i++){
+			double sq=pow(i, 0.5);
+			psum+=sq;
+		
+	}
+	pthread_mutex_lock(&sumlock);
+	ssum=ssum+psum;
+	pthread_mutex_unlock(&sumlock);
 
 }
 
 int main(int argc, char *argv[]){
-if (argc<2){
+	if (argc<2){
 	printf("No number entered");
 	return EXIT_FAILURE;
-}
+	}
 
-int n = atoi(argv[1]);
-printf("\n%i\n",n);
-pthread_t t1, t2;
-/*
-start_points[0]=1;
-start_points[1]=n/3+1;
-*/
-start_points[2]=2*n/3+1;
-start_points[3]=n+1;
-printf("\n%i\n",start_points[2]);
-printf("\n%i\n",start_points[3]);
-/*pthread_create(&t1, NULL, sumSqrt(start_points[1], start_points[2]), NULL);*/
-double res=sumSqrt(start_points[2], start_points[3]);
-//pthread_create(&t2, NULL, sumSqrt(start_points[2], start_points[3]), NULL);
-/*sumSqrt(start_points[0], start_points[1]);
-pthread_join(t1,NULL);
-pthread_join(t2,NULL);
-//printf("\nThe sum of the sqrts of %i is %d", n, ssum);
-*/
+	int n = atoi(argv[1]);
+	printf("\n%i\n",n);
+	pthread_t t1, t2;
 
-printf("%d\n", ssum);
-return 0;
+	sp1[0]=1;
+	sp1[1]=n/3+1;
+	
+	sp2[0]=n/3+1;
+	sp2[1]=2*n/3+1;
+	
+	sp3[0]=2*n/3+1;
+	sp3[1]=n+1;
+
+
+
+
+	pthread_create(&t1, NULL, sumSqrt, sp2);
+	pthread_create(&t2, NULL, sumSqrt, sp3);
+	sumSqrt(sp1);
+
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	printf("\nThe sum of the sqrts of %i is %d", n, ssum);
+
+
+	
+	return 0;
 }
